52c701674a36e4f7fd85cd5d35751532cfcf0f5a
David Winslow <cdwinslow@gmail.com>
2012-08-04 01:30:35 -0400

    Allow alternative query at command line

--- Main.hs
+++ Main.hs
@@ -1,14 +1,9 @@
 module Main where
 
+import System.Environment
 import Wikipedia hiding (main)
 import Doxament
 import Parse
-
-text :: String
-text = "Uruguay is not in South America"
-
-terms :: [Term]
-terms = removeStops $ concat $ sentences text
 
 justs :: [Maybe a] -> [a]
 justs (Just a : rest)  = a : justs rest
@@ -16,12 +11,25 @@ justs (Nothing : rest) = justs rest
 justs [] = []
 
 buildCorpus :: [Maybe String] -> Doxament
-buildCorpus articles = foldl (\corpus a -> merge corpus $ relations a) [] $ justs articles 
+buildCorpus articles = foldl add [] articles'
+    where add corpus a = merge corpus $ relations a
+          articles' = justs articles
+
+noisyFetchArticle :: String -> IO (Maybe String)
+noisyFetchArticle topic = do putStrLn $ "Looking up " ++ topic
+                             fetchArticle "simple" topic
+
+queryFromArgs :: String -> [String] -> String
+queryFromArgs def args | null args = def
+                       | otherwise = head args
 
 main :: IO ()
-main = do articles <- mapM (fetchArticle "simple") terms
+main = do args <- getArgs
+          let q = queryFromArgs "Uruguay is not in South America." args
+          let terms = (removeStops . concat . sentences) q
+          articles <- mapM noisyFetchArticle terms
           putStrLn "Building knowledge base"
           let corpus = buildCorpus articles
           putStrLn "Querying against original document"
-          let res = query corpus $ relations text
+          let res = query corpus $ relations q
           print res
