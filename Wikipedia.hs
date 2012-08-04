7203df9d2ad92e1989d5bcb97a28f846d0f9e2cd
David Winslow <cdwinslow@gmail.com>
2012-08-03 23:24:24 -0400

    Follow redirects from Wikipedia articles

--- Wikipedia.hs
+++ Wikipedia.hs
@@ -1,31 +1,36 @@
 module Wikipedia where
 
-import Text.Printf
+import Data.Array
+-- import Data.Maybe
 import Network.HTTP
 import Network.Stream
-
-articleUrl :: String -> String -> String
-articleUrl = printf "http://%s.wikipedia.org/w/index.php?action=raw&title=%s"
-
-fetchArticle :: String -> String -> IO (Maybe String)
-fetchArticle lang topic = do resp <- (simpleHTTP . getRequest) url
-                             let body = asMaybe resp
-                             resolved <- resolveWikiRedirect body
-                             return body
-    where url = articleUrl lang topic
-
-wikiRedirect :: String -> Maybe String
-wikiRedirect page = if "#redirect" `isPrefixOf` lpage 
-                    then drop
-
-resolveWikiRedirect :: Maybe String -> IO (Maybe String)
-resolveWikiRedirect Nothing = return Nothing
-resolveWikiRedirect page =
+import Text.Printf
+import Text.Regex.PCRE
 
 asMaybe :: Result (Response a) -> Maybe a
 asMaybe = either (const Nothing)
                  (Just . rspBody)
 
+articleUrl :: String -> String -> String
+articleUrl = printf "http://%s.wikipedia.org/w/index.php?action=raw&title=%s"
+
+redirectPattern :: Regex
+redirectPattern = makeRegex "(?i)#REDIRECT \\[\\[([^[\\]]+)\\]\\]"
+
+redirectTopic :: String -> Maybe String
+redirectTopic page = fmap capture $ matchOnceText redirectPattern page
+    where capture (_, matches, _) = fst $ matches ! 1
+
+fetchArticle :: String -> String -> IO (Maybe String)
+fetchArticle lang topic =
+    do resp <- (simpleHTTP . getRequest) $ articleUrl lang topic
+       let page = asMaybe resp
+       case page >>= redirectTopic of 
+           Just topic' -> fetchArticle lang topic'
+           Nothing -> return page
+
 main :: IO ()
-main = do res <- fetchArticle "simple" "Article"
-          print res
+main = do res <- fetchArticle "simple" "..."
+          case res of
+              Nothing -> return ()
+              Just page -> putStrLn page
