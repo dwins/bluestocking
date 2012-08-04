1c157aea1821ad21796c1be7dc4c058ea27c3f54
David Winslow <cdwinslow@gmail.com>
2012-08-04 00:20:18 -0400

    Add paragraph splitting for wikipedia

--- Wikipedia.hs
+++ Wikipedia.hs
@@ -14,11 +14,21 @@ asMaybe = either (const Nothing)
 articleUrl :: String -> String -> String
 articleUrl = printf "http://%s.wikipedia.org/w/index.php?action=raw&title=%s"
 
+breakOn :: String -> String -> [String]
+breakOn pattern text =
+   case matchOnce (makeRegex pattern :: Regex) text of
+       Nothing -> [text]
+       Just matches ->
+           let (offset, len) = matches ! 0
+               (h, t) = splitAt offset text
+           in h : (breakOn pattern . drop len) t
+
 capture :: String -> Int -> String -> String
 capture pattern n text = foldr id text transforms
    where transforms = map toTransform $ matchAllText (makeRegex pattern :: Regex) text
          toTransform ms = matchTransform (ms ! 0) (ms ! n)
          matchTransform (_, (offset, len)) (sub, _) = replaceRange offset len sub
+
 subst :: String -> String -> String -> String
 subst pattern sub text = foldr id text transforms
    where transforms = map toTransform $ matchAll (makeRegex pattern :: Regex) text
@@ -73,8 +83,12 @@ unwiki = capture "(?i)\\{\\{IPA(?:\\-[^|{}]+)*?\\|([^|{}]+)(\\|[^{}]+)*?\\}\\}" 
          remove "''+" .
          remove "(?m)^\\*$"
 
+paragraphs :: String -> [String]
+paragraphs = breakOn "\\n\\n+" . cleanWhitespace 
+    where cleanWhitespace = subst "\\r\\n|\\n|\\r" "\n"
+
 main :: IO ()
 main = do res <- fetchArticle "simple" "..."
           case res of
               Nothing -> return ()
-              Just page -> putStrLn . unhtml . unwiki $ page
+              Just page -> putStrLn $ head . paragraphs . unhtml . unwiki $ page
