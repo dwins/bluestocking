0d5f47b840de86cd1d2b34357325dc9c8e4bb134
David Winslow <cdwinslow@gmail.com>
2012-08-04 00:09:03 -0400

    Implement markup (html and wikipedia) stripping

--- Wikipedia.hs
+++ Wikipedia.hs
@@ -14,12 +14,37 @@ asMaybe = either (const Nothing)
 articleUrl :: String -> String -> String
 articleUrl = printf "http://%s.wikipedia.org/w/index.php?action=raw&title=%s"
 
+capture :: String -> Int -> String -> String
+capture pattern n text = foldr id text transforms
+   where transforms = map toTransform $ matchAllText (makeRegex pattern :: Regex) text
+         toTransform ms = matchTransform (ms ! 0) (ms ! n)
+         matchTransform (_, (offset, len)) (sub, _) = replaceRange offset len sub
+subst :: String -> String -> String -> String
+subst pattern sub text = foldr id text transforms
+   where transforms = map toTransform $ matchAll (makeRegex pattern :: Regex) text
+         toTransform = matchTransform . (! 0)
+         matchTransform (offset, len) = replaceRange offset len sub
+
+remove :: String -> String -> String
+remove pattern text = foldr id text transforms
+   where transforms = map toTransform $ matchAll (makeRegex pattern :: Regex) text
+         toTransform = matchTransform . (! 0)
+         matchTransform (offset, len) = removeRange offset len
+
+removeRange :: Int -> Int -> String -> String
+removeRange i n text = h ++ drop n t
+    where (h, t) = splitAt i text
+
+replaceRange :: Int -> Int -> String -> String -> String
+replaceRange i n sub text = h ++ sub ++ drop n t
+    where (h, t) = splitAt i text
+
 redirectPattern :: Regex
 redirectPattern = makeRegex "(?i)#REDIRECT \\[\\[([^[\\]]+)\\]\\]"
 
 redirectTopic :: String -> Maybe String
-redirectTopic page = fmap capture $ matchOnceText redirectPattern page
-    where capture (_, matches, _) = fst $ matches ! 1
+redirectTopic page = fmap getCapture $ matchOnceText redirectPattern page
+    where getCapture (_, matches, _) = fst $ matches ! 1
 
 fetchArticle :: String -> String -> IO (Maybe String)
 fetchArticle lang topic =
@@ -29,8 +54,27 @@ fetchArticle lang topic =
            Just topic' -> fetchArticle lang topic'
            Nothing -> return page
 
+unhtml, unwiki :: String -> String
+unhtml = subst "(?i)&nbsp;" " " .
+         subst "(?i)<br[ \\\\]*?>" "\n" .
+         subst "(?i)&amp;" "&" .
+         remove "(?m)<!--.*?--\\s*>" .
+         remove "(?i)<ref[^>]*>[^>]*<\\/ ?ref>" .
+         remove "(?m)<.*?>"
+
+unwiki = capture "(?i)\\{\\{IPA(?:\\-[^|{}]+)*?\\|([^|{}]+)(\\|[^{}]+)*?\\}\\}" 1 .
+         capture "(?i)\\{\\{Lang(\\-[^|{}]+)*?\\|([^|{}]+)(\\|[^{}]+)*?\\}\\}" 1 .
+         capture "\\[(.*?)\\|.*?\\]" 1 .
+         capture "\\[\\[[^[\\]]*?\\|([^[\\]]*?)\\]\\]" 1 .
+         capture "\\[\\[([^[\\]]+?)\\]\\]" 1 .
+         capture "\\[[^[\\]]*? ([^[\\]]*?)\\]" 1 .
+         remove "(?m)\\{\\{[^{}]+\\}\\}" .
+         remove "(?i)\\[\\[(?:Category|Image|File):[^[\\]]*?\\]\\]" .
+         remove "''+" .
+         remove "(?m)^\\*$"
+
 main :: IO ()
 main = do res <- fetchArticle "simple" "..."
           case res of
               Nothing -> return ()
-              Just page -> putStrLn page
+              Just page -> putStrLn . unhtml . unwiki $ page
